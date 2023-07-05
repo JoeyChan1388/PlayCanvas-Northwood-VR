4b371a582447538886fc1acbccdfab72fc089e09
Joey Chan <61027406+JoeyChan1388@users.noreply.github.com>
2023-07-05 15:53:50 -0400

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1786640.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 140682001, 140682174, 140682008, 140682094, 140682159, 140682230, 140682179, 140682184, 140681998, 140682189, 140682170, 140682193, 140682186, 140682185, 140681924, 140682114, 140682010, 140682150, 140682188, 140681930, 140681925, 140682157, 140689487 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
